02b8019c7acdcaa765a58d523a00087ed1f08037
Jordon Dueck <jordon.dueck@gmail.com>
2019-12-17 21:32:48 -0800

    Improve card styling

--- src/components/Categories/FilmsCard.jsx
+++ src/components/Categories/FilmsCard.jsx
@@ -2,10 +2,20 @@ import React from "react";
 import "./Card.css"
 
 const FilmsCard = props => {
+  // console.log('props' , props);
+  let results = Object.values(props);
+  let description = results[2];
+  let readMore = null;
+
+  if (description.length >= 250) {
+    description = description.slice(0, 250);
+    readMore = <a href='#'>(...read more)</a>
+  }
+  
   return (
     <ul className="card">
       <li><h2>{props.title}</h2></li>
-      <li><b>Description:</b> {props.description}</li>
+      <li><b>Description:</b> {description} {readMore}</li>
       <li><b>Director:</b> {props.director}</li>
       <li><b>Producer:</b> {props.producer}</li>
       <li><b>Release Date:</b> {props.release_date}</li>
