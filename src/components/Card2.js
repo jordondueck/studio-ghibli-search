f842c937bef1156fb0cef95c4a0dcf97501e09b8
Jordon Dueck <jordon.dueck@gmail.com>
2019-12-20 02:07:59 -0800

    Add home icon to navbar and units to applicable search results

--- src/components/Card2.js
+++ src/components/Card2.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./categories/Category.css"
+import tomato from "../images/tomato-576798.svg";
 
 // Change to Card
 const Card2 = props => {
@@ -9,16 +10,16 @@ const Card2 = props => {
       { name: 'director', label: 'Director' },
       { name: 'producer', label: 'Producer' },
       { name: 'release_date', label: 'Release Date' },
-      { name: 'rt_score', label: 'Rotten Tomatoes Score' }
+      { name: 'rt_score', label: 'Rotten Tomatoes Score', unit: 'out of 100' }
     ],
     locations: [
       { name: 'climate', label: 'Climate' },
       { name: 'terrain', label: 'Terrain' },
-      { name: 'surface_water', label: 'Surface Water' }
+      { name: 'surface_water', label: 'Surface Water', unit: 'km' }
     ],
     people: [
       { name: 'gender', label: 'Gender' },
-      { name: 'age', label: 'Age' },
+      { name: 'age', label: 'Age', unit: 'years' },
       { name: 'eye_color', label: 'Eye Color' },
       { name: 'hair_color', label: 'Hair color' }
     ],
@@ -29,17 +30,16 @@ const Card2 = props => {
     vehicles: [
       { name: 'description', label: 'Description' },
       { name: 'vehicle_class', label: 'Vehicle Class' },
-      { name: 'length', label: 'Length' }
+      { name: 'length', label: 'Length', unit: 'm' }
     ]
   }
 
   const selectedCategory = categoryFields[(props.category).toLowerCase()];
   const cardTitle = <li id='-1'><h2>{props.name ? props.name : props.title}</h2></li>;
-  const readMore = <a href='#'>(...read more)</a>
+  const readMore = <a href='#'>(read more)</a>
 
   return (
     <ul className="card">
-      {/* DEV LOG */}
       {/* {console.log('props' , props)}
       {console.log('selectedCategory' , selectedCategory)} */}
       {cardTitle}
@@ -51,7 +51,22 @@ const Card2 = props => {
           } else {
             return <li id={index}>{props[value.name].slice(0, 250)} {readMore}</li>
           }
-        } else {
+        } else if (value.unit) { // If the value has a unit (set as state variable)
+          if (value.name === 'age' && isNaN(props[value.name])) { // Special Case - do not use the unit (years) when age is empty or N/A
+            return <li id={index}><h7>{value.label}:</h7> {props[value.name]}</li>
+          } else if (value.name === 'rt_score') { // Special Case - add "rotten tomato" graphic as the unit
+            const rottenTomatoes = <img className="rotten-tomato" src={tomato} alt='rotten tomato'></img>;
+            return <li id={index}><h7>{value.label}:</h7> {props[value.name]} {rottenTomatoes}</li>
+          } else if (value.name === 'surface_water') { // Special Case - only use km^2 when surface water is greater than 0
+            if (props[value.name] > 0) {
+              return <li id={index}><h7>{value.label}:</h7> {props[value.name]} {value.unit}<sup>2</sup></li>
+            } else if (props[value.name] === 0) {
+              return <li id={index}><h7>{value.label}:</h7> None</li>
+            }
+          } else {  // Default Case - append the unit to the value (if unit is set)
+            return <li id={index}><h7>{value.label}:</h7> {props[value.name]} {value.unit}</li>
+          }
+        } else {  // If the value does not have a unit (set as a state variable)
           return <li id={index}><h7>{value.label}:</h7> {props[value.name]}</li>
         }
       })}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./NavBar.css";
+import homeButton from "../images/home-24px.svg";
 
 const NavBar = ({ category, handleNav, handleHome }) => {
   return (
@@ -8,11 +9,12 @@ const NavBar = ({ category, handleNav, handleHome }) => {
         <h3 onClick={handleHome}>Studio Ghibli Search</h3>
       </div>
       <ul className="nav-links">
-      <li key={-1} id={-1} onClick={handleHome}><b>Home</b></li>
-        {category.map((name, i) => {
+      {/* <li key='-1' id='-1' onClick={handleHome}><b>Home</b></li> */}
+      <li className="home-link" onClick={handleHome}><img src={homeButton} alt='Home'></img></li>
+        {/* {category.map((name, i) => {
           // return <li className="nav-item" key={i} value={category[i]} onClick={handleNav}><a href="#">{name}</a></li>;
           return <li key={i} id={i} onClick={handleNav}>{name}</li>;
-        })}
+        })} */}
         {/* <li key={-1} id={-1} onClick={handleHome}>Home</li> */}
       </ul>
     </nav>
